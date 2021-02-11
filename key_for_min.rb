ba1f173f15e171293bee366781527f03873407b0
Susana vik <susana.m.vik@gmail.com>
2021-02-11 20:52:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_key = nil
   name_hash.each do |key, value|
-    if owest_value == nil || value < lowest_value
+    if lowest_value == nil || value < lowest_value
       lowest_value = value
       lowest_key = key
   end
